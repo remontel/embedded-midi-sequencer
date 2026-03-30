5e7e2989b2e2f2e2ad4d9763fce8f7749604222f
remontel <re.montelongo@gmail.com>
2026-03-30 14:46:16 -0700

    Update timer_seq.c starter code

--- src/timer_seq.c
+++ src/timer_seq.c
@@ -1,11 +1,6 @@
 /**
  * @file timer_seq.c
- * @brief Timer configuration and interrupt-based step timing engine.
- *
- * This file configures a hardware timer to generate periodic interrupts
- * based on the sequencer BPM setting. The timer interrupt service routine
- * advances the playback step and signals the rest of the system when
- * real-time sequencer events occur.
+ * @brief Hardware timer implementation for sequencer timing.
  */
 
 
@@ -15,26 +10,8 @@
 
 void TimerSeq_Init(uint16_t bpm)
 {
-    uint32_t reload_value;
-
-    if (bpm == 0U)
-    {
-        bpm = 1U;
-    }
-
-    reload_value = (SystemCoreClock / ((uint32_t)bpm * 4U)) - 1U;
-
-    SYSCTL->RCGCTIMER |= (1UL << 0);
-    (void)SYSCTL->RCGCTIMER;
-
-    TIMER0->CTL &= ~(1UL << 0);
-    TIMER0->CFG = 0x0U;
-    TIMER0->TAMR = 0x2U;
-    TIMER0->TAILR = reload_value;
-    TIMER0->ICR = (1UL << 0);
-    TIMER0->IMR |= (1UL << 0);
-
-    NVIC_EnableIRQ(TIMER0A_IRQn);
+    (void)bpm;
+    /* TODO: Configure timer peripheral, load interval, enable interrupt */
 }
 
 void TimerSeq_UpdatePeriod(uint16_t bpm)
@@ -45,7 +22,7 @@ void TimerSeq_UpdatePeriod(uint16_t bpm)
 
 void TimerSeq_Start(void)
 {
-    TIMER0->CTL |= 0x1U;
+    /* TODO: Enable timer counting */
 }
 
 void TimerSeq_Stop(void)
